6c7983b82574b0dbefeef6e70bba622e0d808b7d
Rasmus Tinndahn <s144002@student.dtu.dk>
2019-01-08 16:43:51 +0100

    Added block processing and internal mixer
    Internal mixer mixes between current non-processed input and an effected
    signal delayed by FFTSIZE

--- to bela/render.cpp
+++ to bela/render.cpp
@@ -6,25 +6,38 @@
 #include <math_neon.h>
 
 // AUDIO VARIABLES
-#define gAUDIOFRAMES 128
-int gAudioChannelNum; // number of audio channels to iterate over
+
+// Setup by user
+#define gAUDIOFRAMES 1024
+#define FFTSIZE 128
+float gGainClean_dB = 0;
+float gGainFX_dB = 0;
 int gAudioChannelInput = 0; //which input channel to use. 0 = left, 1 = right
 
-float gBlock[gAUDIOFRAMES]; // AudioBlock to be used in render.
+
+
+// Internal only
+int gAudioChannelNum; // number of audio channels to iterate over
+float gBlock[2*FFTSIZE]; // AudioBlock to be used in render. first FFTSIZE is for input buffering, last FFTSIZE for output
+float gBlockClean[gAUDIOFRAMES]; // Block to be used for clean signal
+float gOutPutBuffer[gAUDIOFRAMES];
+float gGainClean_lin = pow(10, gGainClean_dB / 20.0);
+float gGainFX_lin = pow(10, gGainFX_dB / 20.0);
+int gStatesTotal = FFTSIZE/gAUDIOFRAMES; // Amount of states needed 
+int gState = 0; // Current state
 
 
 
-// TEST
-float test[gAUDIOFRAMES];
+// TEST VARIABLES
 
 // Variables used to print every gInterval during render
-float gInterval = 1;
+float gInterval = 1; 
 float gSecondsElapsed = 0;
 int gCount = 0;
 
 
 // ------------------------- USER SETTINGS -------------------------
-void Bela_userSettings(BelaInitSettings *settings)
+void Bela_userSettings(BelaInitSettings *settings) 
 {
 	settings->interleave = 0; //Sets if the audio buffers are interleaved or not. 0 = not interleaved.
 	settings->analogOutputsPersist = 0; //makes sure analog outs do not persist across frames. Reduces memory usage
@@ -34,77 +47,49 @@ void Bela_userSettings(BelaInitSettings *settings)
 // ------------------------- SETUP -------------------------
 bool setup(BelaContext *context, void *userData)
 {
-
     // Print strings
-    rt_printf("Using input %s \n", gAudioChannelInput ? "Right":"Left");
+    rt_printf("Using %s input\n", gAudioChannelInput ? "Right":"Left");
 	rt_printf("Audioframes = %d samples per input \n",context->audioFrames);
 	rt_printf("Bela buffers are %s\n", (context->flags & BELA_FLAG_INTERLEAVED) ? "interleaved":"not interleaved");
-
-
+	rt_printf("Clean audio is at %f dB = %f lin \n",gGainClean_dB,gGainClean_lin);
+	rt_printf("FX audio is at %f dB = %f lin \n",gGainFX_dB,gGainFX_lin);
 	// Set number of audio channels to minimum of I/O
     gAudioChannelNum = std::min(context->audioInChannels, context->audioOutChannels);
-
-
-
+ 
 	return true;
-}
+} //END setup
 
 
 // ------------------------- RENDER -------------------------
 void render(BelaContext *context, void *userData)
 {
-
-	// Copies input channel to processing block
-	memcpy(gBlock, &context->audioIn[gAudioChannelInput * context->audioFrames],sizeof(context->audioIn)*context->audioFrames);
-
-
-
-
-
-/*	for(unsigned int n = 0; n < context->audioFrames; n++) { //main audio loop if not using block
-
-		for(unsigned int ch = 0; ch < gAudioChannelNum; ch++){
-
-
-
-			audioWriteNI(context, n, ch, audioReadNI(context, n, ch));
-
-
-		} //end inner audio loop
-
-		// Increment a counter on every frame
-		gCount++;
-
-		// Print a message every interval
-		if(gCount % (int)(context->audioSampleRate*gInterval) == 0) {
-		    gSecondsElapsed += gInterval;
-
-
-
-	    	rt_printf("memcpy: ");
-		    for(int j = 0;j<context->audioFrames;j++){
-				rt_printf("%f, ",gBlock[j]);
-		    }
-		    rt_printf("\n audioRead: ");
-		    for(int j = 0;j<context->audioFrames;j++){
-				rt_printf("%f, ",test[j]);
-		    }
-		}
-
-
-
-	} //end outer audio loop
-*/
-
-	memcpy(context->audioOut,gBlock,sizeof(context->audioIn)*context->audioFrames);	//Copy block to left output channel
-	memcpy(&context->audioOut[1 * context->audioFrames],gBlock,sizeof(context->audioIn)*context->audioFrames);  //Copy block to right output channel
-
-}
-
-
+	// Copies input channel to processing block and a block holding latest clean signal
+	memcpy(&gBlock[gState * gAUDIOFRAMES], &context->audioIn[gAudioChannelInput * context->audioFrames],sizeof(context->audioIn)*context->audioFrames);
+	memcpy(gBlockClean, &context->audioIn[gAudioChannelInput * context->audioFrames],sizeof(context->audioIn)*context->audioFrames);
+		
+	
+	
+	// Mix clean and processed fx signal
+	for(unsigned int idx = 0; idx <gAUDIOFRAMES;idx++){ 
+		gOutPutBuffer[idx] =gGainFX_lin*gBlock[FFTSIZE + gState*gAUDIOFRAMES + idx] + gGainClean_lin* gBlockClean[idx];
+	}
+	
+	// Send total signal to outputs
+	memcpy(context->audioOut,gOutPutBuffer,sizeof(context->audioIn)*context->audioFrames);	//Copy output block to left output channel
+	memcpy(&context->audioOut[1 * context->audioFrames],gOutPutBuffer,sizeof(context->audioIn)*context->audioFrames);  //Copy output block to right output channel
+	
+	
+	// Increment state and copy a full input buffer to processing
+	gState++;
+	if(gState >= gStatesTotal){
+		gState = 0;
+		memcpy(&gBlock[FFTSIZE], &gBlock[0],sizeof(context->audioIn)*FFTSIZE);
+	}
+} // END render
 
 // ------------------------- CLEANUP -------------------------
 void cleanup(BelaContext *context, void *userData)
 {
 
 }
+
